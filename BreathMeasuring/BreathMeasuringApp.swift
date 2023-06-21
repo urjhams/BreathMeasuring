9e1c473a546c597fa57db136c68ae5ea9b4e347a
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-06-21 03:42:40 +0200

    init

---
+++ BreathMeasuring/BreathMeasuringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathMeasuringApp.swift
+//  BreathMeasuring
+//
+//  Created by Quân Đinh on 21.06.23.
+//
+
+import SwiftUI
+
+@main
+struct BreathMeasuringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
