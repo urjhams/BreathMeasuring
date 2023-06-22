4bdba61b4cba904b4849e2ec6d2483f40ff78dc4
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-06-22 06:28:15 +0200

    receive publisher instead

--- BreathMeasuring/Views/ContentView.swift
+++ BreathMeasuring/Views/ContentView.swift
@@ -13,40 +13,14 @@ struct ContentView: View {
   // breath observer
   @ObservedObject private var breathObserver = BreathObsever()
   
-  // timer
-  let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
-    
-  @State var isTrackingBreath = false {
-    didSet {
-      if isTrackingBreath {
-        breathObserver.startTrackAudioSignal()
-      } else {
-        breathObserver.stopTrackAudioSignal()
-      }
-    }
-  }
-  
   var body: some View {
     VStack {
-      
       // https://developer.apple.com/documentation/charts/creating-a-chart-using-swift-charts
-      Button(isTrackingBreath ? "Stop monitoring" : "Start monitoring") {
-        isTrackingBreath.toggle()
+      Button(breathObserver.isTracking ? "Stop monitoring" : "Start monitoring") {
+        breathObserver.isTracking.toggle()
       }
-      .onReceive(timer) { _ in
-        guard isTrackingBreath else {
-          return
-        }
-        
-        do {
-          let value = try breathObserver.trackAudioSignal()
-          // TODO: add the live graph based on the value from breath Observer
-          // TODO: find the threshold, frequency of breathing
-          // TODO: make the breath pattern measuring by getting peak
-          print("🙆🏻🙆🏻🙆🏻 \(value)")
-        } catch {
-          print("error: \(error.localizedDescription)")
-        }
+      .onReceive(breathObserver.$convertedPowerLevel) { powerLevel in
+        print("🙆🏻🙆🏻🙆🏻 \(powerLevel)")
       }
       
     }
