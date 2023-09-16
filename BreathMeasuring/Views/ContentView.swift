d95085c723352975207ee6bd8f6c3e752eda1ede
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-09-16 22:08:59 +0200

    try new BreathObserver

--- BreathMeasuring/Views/ContentView.swift
+++ BreathMeasuring/Views/ContentView.swift
@@ -7,67 +7,23 @@
 
 import SwiftUI
 import BreathObsever
-import Combine
-
-let updateCycle: TimeInterval = 0.05
-
-var analyzingTime: TimeInterval = 5 * 60
 
 struct ContentView: View {
   
-  @State var timer = Timer
-    .publish(every: updateCycle, on: .main, in: .default)
-    .autoconnect()
-    .eraseToAnyPublisher()
-  
-  @State var tracking: Bool = false
-  
-  @State var logText: String = ""
-  
+  @State var running = false
+
   // breath observer
-  @ObservedObject private var breathObserver = BreathObsever(cycle: updateCycle, end: analyzingTime)
+  private var breathObserver = BreathObsever()
   
   var body: some View {
     VStack {
       
-      // TODO: maybe we could use a threshold to decice is that sounds still in breathing sound range (like under 200)
-      LineChartView(
-        timer: timer,
-        latestData: $breathObserver.convertedPowerLevel,
-        dataRange: 1000,
-        tracking: $tracking
-      )
-      .frame(height: 400)
       
-      // TODO: button will start a session
-      // https://developer.apple.com/documentation/charts/creating-a-chart-using-swift-charts
-      Button(breathObserver.isTracking ? "Stop monitoring" : "Start monitoring") {
-        do {
-          if breathObserver.isTracking {
-            breathObserver.deallocateTimer()
-          } else {
-            try breathObserver.assignTimer(timer: timer)
-          }
-        } catch {
-          logText += "\n❗️ \(error.localizedDescription)"
-        }
-        
-        guard breathObserver.hasTimer else {
-          return breathObserver.deallocateTimer()
-        }
-        
-        breathObserver.isTracking.toggle()
-        tracking = breathObserver.isTracking
+      Button(running ? "Stop" : "Start") {
+        running.toggle()
+        running ? breathObserver.startProcess() : breathObserver.stopProcess()
       }
-      .foregroundColor(breathObserver.sessionAvailable ? .blue : .red)
-      
-      TextEditor(text: $logText)
-        .padding(.all, 8)
-        .cornerRadius(3)
-      
-      // TODO: implement something relate to applw watch
-      // TODO: combine with cognitive load result from BreathObserver
-      // TODO: add the 2 results above to the session (as CSV(?)) and store on Firebase.
+
     }
     .padding()
   }
