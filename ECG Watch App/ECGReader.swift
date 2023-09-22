ece9ab394092f31a67414e110f972779fe3d9a80
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-09-22 22:07:35 +0200

    heart rate observation

--- ECG Watch App/ECGReader.swift
+++ ECG Watch App/ECGReader.swift
@@ -6,20 +6,17 @@ public final class ECGReader: NSObject, ObservableObject {
   
   @Published public var isAvailable = false
   
-  @Published public var value = 0
+  @Published public var value = 0.0
   
-  let bpm = HKUnit(from: "count/min")
-  
-  var healthStore: HKHealthStore?
+  private var healthStore: HKHealthStore?
   
   public override init() {
-    super.init()
-    authorize()
+
   }
 }
 
 extension ECGReader {
-  private func authorize() {
+  public func start() {
     
     guard HKHealthStore.isHealthDataAvailable() else {
       return
@@ -44,5 +41,44 @@ extension ECGReader {
           return
         }
       }
+    
+    // process the queries
+    process()
   }
+  
+  public func stop() {
+    healthStore = nil
+  }
+}
+
+extension ECGReader {
+  
+  private func process() {
+    // Set up the predicate source from current device (Apple watch)
+    let device = HKDevice.local()
+    let predicate = HKQuery.predicateForObjects(from: [device])
+    
+    guard let sampleType = HKObjectType.quantityType(forIdentifier: .heartRate) else {
+      isAvailable = false
+      return
+    }
+    
+    let query = HKAnchoredObjectQuery(
+      type: sampleType,
+      predicate: predicate,
+      anchor: nil,
+      limit: HKObjectQueryNoLimit
+    ) { query, samples, deletedObjects, anchor, error in
+      samples?.forEach { [weak self] sample in
+        guard let sample = sample as? HKQuantitySample else {
+          return
+        }
+        let hearRateQuantity = HKUnit(from: "count/min")
+        self?.value = sample.quantity.doubleValue(for: hearRateQuantity)
+      }
+    }
+    
+    healthStore?.execute(query)
+  }
+  
 }
