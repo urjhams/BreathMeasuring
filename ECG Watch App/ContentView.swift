d0d5d92b5222439de306b000ee25eef6a4b85f5b
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-09-22 20:57:46 +0200

    add watch extension

---
+++ ECG Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ECG Watch App
+//
+//  Created by Quân Đinh on 22.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ECG Watch App/ECGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECGApp.swift
+//  ECG Watch App
+//
+//  Created by Quân Đinh on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ECG_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
