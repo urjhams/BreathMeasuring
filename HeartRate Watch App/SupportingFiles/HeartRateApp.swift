f8b6e0e7950287bde478a679e6aebef56e29a6fc
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-09-23 00:24:31 +0200

    re-create Watch side target

---
+++ HeartRate Watch App/SupportingFiles/HeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateApp.swift
+//  HeartRate Watch App
+//
+//  Created by Quân Đinh on 23.09.23.
+//
+
+import SwiftUI
+
+@main
+struct HeartRate_Watch_AppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
